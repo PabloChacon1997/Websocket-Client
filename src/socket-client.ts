99f4678f5ca3993a588cd09a2d0a317db61a5c2f
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-11-13 22:02:12 -0500

    add input to messages and jwt

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,13 @@
 import { Manager, Socket } from 'socket.io-client'
 
 
-export const connectToServer = () => {
-  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+export const connectToServer = (token: string) => {
+  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+      hola: 'mundo',
+      authentication: token,
+    }
+  });
   const socket = manager.socket('/');
   addListeners(socket);
 }
@@ -11,6 +16,12 @@ export const connectToServer = () => {
 const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector('#server-status')!;
   const clientsUl = document.querySelector('#clients-ul')!;
+  
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form');
+  const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+
   socket.on('connect', () => {
     serverStatusLabel.innerHTML = 'connected';
   });
@@ -28,5 +39,25 @@ const addListeners = (socket: Socket) => {
     })
 
     clientsUl.innerHTML = clientsHtml;
+  });
+
+  messageForm?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (messageInput.value.trim().length <= 0) return;
+    socket.emit('mesage-from-client',{id: 'Yo', message: messageInput.value});
+    messageInput.value = '';
   })
+
+  socket.on('messages-from-server', (payload: { fullName: string, message: string }) => {
+    const newMessage = `
+      <li>
+        <strong>${payload.fullName}</strong>
+        <span>${payload.message}</span>
+      </li>
+    `;
+
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+    messagesUl.append(li);
+  });
 }
